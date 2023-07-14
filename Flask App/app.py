dd07848d261f9f0ecabf1445d96a1700be279545
Tejinder Soomal <tss122@ic.ac.uk>
2023-07-14 14:13:00 +0100

    Added Bootstrap templates folder. Created shell website with home page and about page, as well as a navbar.

--- Flask App/app.py
+++ Flask App/app.py
@@ -1,9 +1,36 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+# Initialising the database
+db = SQLAlchemy(app)
+app.app_context().push()
+
+# Creating the database model
+class Test(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # Create function to return string when we add something
+    def __repr__(self):
+        return "<Name %r>" % self.id
+
 
 @app.route("/")
 def main():
-    return render_template('index.html')
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+# @app.route("/testdb")
+# def testdb():
+#     title = "testdb Database"
+#     return render_template("test.html", title=title)
 
 if __name__ == "__main__":
     app.run()
