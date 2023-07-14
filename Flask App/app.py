7751286ac34627d5331c7499fa9b2785359b9541
Tejinder Soomal <tss122@ic.ac.uk>
2023-07-14 15:59:16 +0100

    Created simple form page with empty box error handling. Goes to form.html after successful submission.

--- Flask App/app.py
+++ Flask App/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -27,14 +27,25 @@ def index():
 def about():
     return render_template("about.html")
 
-@app.route("/queryDatabase")
-def queryDatabase():
-    return render_template("queryDatabase.html")
+@app.route("/query_database")
+def query_database():
+    return render_template("query_database.html")
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
-# @app.route("/showdb")
-# def testdb():
-#     title = "testdb Database"
-#     return render_template("test.html", title=title)
+@app.route("/form", methods=["POST"])
+def form():
+    first_name = request.form.get("first_name")
+    last_name = request.form.get("surname")
+    email_address = request.form.get("email_address")
+
+    if not first_name or not last_name or not email_address:
+        error_statement = "All form fields required!"
+        return render_template("contact.html", error_statement=error_statement, first_name=first_name,
+                               last_name=last_name, email_address=email_address)
+
+    return render_template("form.html", first_name=first_name, last_name=last_name, email_address=email_address)
 
 if __name__ == "__main__":
     app.run()
