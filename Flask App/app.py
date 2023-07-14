e90b9c273e577f525a17c8f2218c164bcddd2d0b
Tejinder Soomal <tss122@ic.ac.uk>
2023-07-14 18:45:36 +0100

    Created basic friends list CRUD using MySQL database.

--- Flask App/app.py
+++ Flask App/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request
+# https://www.youtube.com/playlist?list=PLCC34OHNcOtqJBOLjXTd5xC0e-VD3siPn
+
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import smtplib
@@ -6,13 +8,13 @@ from email.message import EmailMessage
 import os
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///friends.db"
 # Initialising the database
 db = SQLAlchemy(app)
 app.app_context().push()
 
 # Creating the database model
-class Test(db.Model):
+class Friends(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
@@ -36,6 +38,47 @@ def query_database():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+@app.route("/friends", methods =['POST','GET'])
+def friends():
+    if request.method == "POST":
+        friend_name = request.form['name']
+        new_friend = Friends(name=friend_name)
+        # Push to database
+        try:
+            db.session.add(new_friend)
+            db.session.commit()
+            return redirect('/friends')
+        except:
+            return "There was an error adding the friend to the database"
+    else:
+        friends = Friends.query.order_by(Friends.date_created)
+        return render_template("friends.html", friends=friends)
+
+@app.route("/update/<int:id>", methods =['POST','GET'])
+def update(id):
+    friend_to_update = Friends.query.get_or_404(id)
+    if request.method == "POST":
+        friend_to_update.name=request.form['name']
+        # Push to database
+        try:
+            db.session.commit()
+            return redirect('/friends')
+        except:
+            return "There was an error updating that friend in the database"
+    else:
+        return render_template("update.html", friend_to_update=friend_to_update)
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    friend_to_delete = Friends.query.get_or_404(id)
+    try:
+        db.session.delete(friend_to_delete)
+        db.session.commit()
+        return redirect('/friends')
+    except:
+        return "There was a problem deleting that friend"
+
 
 @app.route("/form", methods=["POST"])
 def form():
