8ab7b0c2db4a0985b7a4a519d9d3c4eb1f05cd88
Tejinder Soomal <tss122@ic.ac.uk>
2023-08-05 12:15:50 +0100

    Fixed flask db migrate

---
+++ migrations/versions/eb76ccf83576_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: eb76ccf83576
+Revises: 
+Create Date: 2023-08-05 12:14:44.524236
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'eb76ccf83576'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
